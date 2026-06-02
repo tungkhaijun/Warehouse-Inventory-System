5000d2f7feb627688a87b85d343155fcfa9eb1f3
CHONG YANG YANG <CHONG.YANG.YANG@student.mmu.edu.my>
2026-06-02 16:49:29 +0800

    Add User struct and category field to Product struct

--- structures.h
+++ structures.h
@@ -10,10 +10,19 @@ using namespace std;
 struct Product {
     string productId;
     string productName;
+    string category;
     int quantity;
     double price;
     string location;
     string supplier;
+    Product* next;// Pointer for linked list
+};
+
+struct User {
+    string username;
+    string password;
+    string role;
+    User* next; // Pointer for linked list
 };
 
 #endif
